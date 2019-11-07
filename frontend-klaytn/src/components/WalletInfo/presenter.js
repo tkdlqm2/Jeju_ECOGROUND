125d710297a11aa4d1f8f8a0f774118a5ede2de9
uijin <justwaves@naver.com>
2019-11-07 22:29:16 +0900

    wallet page

---
+++ frontend-klaytn/src/components/WalletInfo/presenter.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import caver from "klaytn/caver";
+import { KLAY_FAUCET } from "constants/url";
+import Input from "components/Input";
+import styled from "styled-components";
+
+const BalanceInput = styled(Input)`
+  font-size: 16px;
+  margin-bottom: 24px;
+`;
+const Link = styled.a`
+  display: inline-block;
+  font-weight: bold;
+  margin-left: 8px;
+  cursor: pointer;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const Container = styled.div``;
+
+const FaucetP = styled.p`
+  font-size: 12px;
+  color: ${props => props.theme.lightGreen};
+`;
+
+export default ({ address, balance }) => {
+  console.log("address:", address);
+  return (
+    <Container>
+      <Input
+        className="WalletInfo__address"
+        name="address"
+        label="Wallet Address"
+        value={address}
+        readOnly
+      />
+      <BalanceInput
+        className="WalletInfo__balance"
+        name="balance"
+        label="Balance"
+        value={`${balance} KLAY`}
+        readOnly
+      />
+      <FaucetP>
+        If you need small amount of Klay for testing.
+        <Link href={KLAY_FAUCET} target="_blank" rel="noreferrer noopener">
+          Run Klay Faucet
+        </Link>
+      </FaucetP>
+    </Container>
+  );
+};
--- frontend-klaytn/src/pages/Wallet.js
+++ frontend-klaytn/src/pages/Wallet.js
@@ -1,5 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+import MakersHeader from "components/MakersHeader";
+import WalletInfo from "components/WalletInfo";
 
-export default () => {
-  return <div></div>;
-};
+const Container = styled.main`
+  width: 100%;
+  min-height: 100%;
+  min-width: ${props => props.theme.maxCardWidth};
+  max-width: ${props => props.theme.maxCardWidth};
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default () => (
+  <Container>
+    <MakersHeader />
+    <WalletInfo />
+  </Container>
+);
